c88bbc4f054902328ded2b18939a8d9bd66aee2d
panky031 <pankajpimpalkar.31@gmail.com>
2021-07-15 15:11:37 +0530

    Create DemoCheck.java

---
+++ DemoCheck.java
@@ -0,0 +1,11 @@
+package demo.github.com;
+
+public class DemoCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Check");
+		System.out.println("DemoCheck");
+	}
+
+}
